c0bbf6cf057ce700607a8fcbac0c74af8f16ca4a
Alejandro <pasill.alej@gmail.com>
2022-09-25 18:04:33 -0500

    Adding all current files

--- CAPSTONE/capstone_application/models.py
+++ CAPSTONE/capstone_application/models.py
@@ -1,3 +1,17 @@
+from audioop import maxpp
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=64)
+    password = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
+    location = models.CharField(max_length=64)
+
+class Pet(models.Model):
+    name = models.CharField(max_length=64)
+    breed = models.CharField(max_length=64)
+    age = models.CharField(max_length=64)
+    size = models.CharField(max_length=64)
+    image = models.ImageField(upload_to="static/")
+    location = models.CharField(max_length=64)
--- CAPSTONE/capstone_application/views.py
+++ CAPSTONE/capstone_application/views.py
@@ -5,17 +5,50 @@ from django.http import HttpResponse
 
 # introduction to page
 def index(request):
+    # Optional: Display globe of world as a heat map of lost pets/ homeless pets
+
+    # display most recent post in cover page with sliding affect 
+
     return render(request, "capstone_application/index.html")
 
 def lost_pets(request):
+    # Get pet information
+
+    # If already in database return error
+
+    # else submit pet information into database
+
+    # If lost display lost page
+
+    # If homeless display homeless page
+
+
     return HttpResponse("Lost_pets Page")   
 
 def login(request):
-    return HttpResponse("logging in to page")
+    # Get username
+
+    # Get password
+
+    # if found in database, login and return home page
+
+    # else, return error and reload login page
+
+    return render(request, "capstone_application/login.html")
 
 def logout(request):
-    return HttpResponse("logging out of page")
+        return render(request, "capstone_application/index.html")
 
 def register(request):
-    return HttpResponse("registering to page")
+    # Get username
+    
+    # Get password
+
+    # Submit username and password to database
+
+    # If succesful return index page
+
+    # If not return register page again
+    
+    return render(request, "capstone_application/register.html")
  
