7c18ec82ca4b2554e12f46011ec2362a43bfa2c9
Alejandro <pasill.alej@gmail.com>
2022-12-12 07:10:15 -0600

    adding current files this morning

--- CAPSTONE/capstone_application/static/Index.js
+++ CAPSTONE/capstone_application/static/Index.js
@@ -1,55 +1,106 @@
+//********************************************************************************************
+//          DomContentLoaded
+//********************************************************************************************
 document.addEventListener('DOMContentLoaded',function() {
     // Load Login page
-    // document.querySelector('#Login').addEventListener('click', () => Login());
+    document.querySelector('#Login').addEventListener('click', () => Login());
     // Load Register page
-    // document.querySelector('#Register').addEventListener('click', () => Register());
-    // Loaf sign out page
-    // document.querySelector('#Logout').addEventListener('click', () => Logout());
+    document.querySelector('#Register').addEventListener('click', () => Register());
+    // Load sign out page
+    document.querySelector('#Logout').addEventListener('click', () => Logout());
     // Load Home page
-    // document.querySelector('#Home').addEventListener('click', () => Home());
-    // Load Lost page
-    // document.querySelector('#Lost').addEventListener('click', () => Lost());
-    // Load Homeless page
-    // document.querySelector('#Homeless').addEventListener('click', () => Homeless());
-    // Load Stories page
-    // document.querySelector('#Stories').addEventListener('click', () => Stories());
+    document.querySelector('#Home').addEventListener('click', () => Home());
+    
+    // Load Mercury page
+    document.querySelector('#mercury').addEventListener('click', () => Mercury());
+    // Load Venus page
+    // document.querySelector('#venus').addEventListener('click', () => Venus());
+    // Load Earth page
+    // document.querySelector('#earth').addEventListener('click', () => Earth());
+    // Load Mars page
+    // document.querySelector('#mars').addEventListener('click', () => Mars());
+    // Load Jupiter page
+    // document.querySelector('#jupiter').addEventListener('click', () => Jupiter());
+    // Load Saturn page
+    // document.querySelector('#Saturn').addEventListener('click', () => Saturn());
+    // Load Uranus page
+    // document.querySelector('#uranus').addEventListener('click', () => Uranus());
+    // Load Neptune page
+    // document.querySelector('#neptune').addEventListener('click', () => Neptune());
+    return false
 })
-// navigate through home page
-function Home(){
-    console.log("made it to the Home")
-}
-// navigate through lost page
-function Lost(){
-    // Create form to allow for people to register animals as lost
-    // Pull API for lost animals in Dallas
-    console.log("made it to the Lost tab")
-}
-// navigate through homeless page
-function Homeless(){
-    // Allow people to post animals seen wondering around
-    // Pull API for Animals needing a home
-    console.log("made it to the Homeless tab")
-}
-// navigate through stories page
-function Stories(){
-    // Allow people to post animals currently being fostered/recovery stories
-    // Pull API for animals beings fostered
-    console.log("made it to the Stories tab")  
-}
+//********************************************************************************************
+//          Login/Logout/Home/Register
+//********************************************************************************************
+
 // Login
 function Login(){
     // allow people to fill out form to login
     console.log("made it to the Login page")
-    
-}
-// Register
-function Register(){
-    // allow people to fill out form to register
-    console.log("made it to the Register page")
-    
 }
 // Sign out
 function Logout(){
     // allow people to sign out from website
     console.log("made it to the Sign out page")
+}
+// navigate through home page
+function Home(){
+    console.log("made it to the Home")
+}
+// Register
+function Register(){
+    // allow people to fill out form to register
+    console.log("made it to the Register page")
+}
+
+
+
+//********************************************************************************************
+//          Planets
+//********************************************************************************************
+// Mercury
+function Mercury(){
+    // allow people to sign out from website
+    console.log("made it to the Mercury page")
+    console.log("made it to the Mercury page")
+    console.log("made it to the Mercury page")
+    console.log("made it to the Mercury page")
+    console.log("made it to the Mercury page")
+    console.log("made it to the Mercury page")
+
+}
+// Venus
+function Venus(){
+    // allow people to sign out from website
+    console.log("made it to the Mercury page")
+}
+// Earth
+function Earth(){
+    // allow people to sign out from website
+    console.log("made it to the Mercury page")
+}
+// Mars
+function Mars(){
+    // allow people to sign out from website
+    console.log("made it to the Mercury page")
+}
+// Jupiter
+function Jupiter(){
+    // allow people to sign out from website
+    console.log("made it to the Mercury page")
+}
+// Saturn
+function Saturn(){
+    // allow people to sign out from website
+    console.log("made it to the Mercury page")
+}
+// Uranus
+function Uranus(){
+    // allow people to sign out from website
+    console.log("made it to the Mercury page")
+}
+// Neptune
+function Neptune(){
+    // allow people to sign out from website
+    console.log("made it to the Mercury page")
 }
